cb5013d82d7f5ed87177bc96f733e21a2d936cdb
RodrigoFdz01 <rodrigof3012@gmail.com>
2022-06-08 12:56:11 -0300

    buttons colors new branch test

--- carrito.js
+++ carrito.js
@@ -28,8 +28,8 @@ items.addEventListener("click", (e) => {
 //addCarrito es el evento
 const addCarrito = (e) => {
   // console.log(e.target);
-  //console.log(e.target.classList.contains("btn-dark"));
-  if (e.target.classList.contains("btn-dark")) {
+  //console.log(e.target.classList.contains("btn-info"));
+  if (e.target.classList.contains("btn-info")) {
     // console.log(e.target.parentElement);
     setCarrito(e.target.parentElement);
   }
@@ -40,7 +40,7 @@ const setCarrito = (objeto) => {
   //console.log(objeto);
   //abajo construyo el obj (producto)
   const producto = {
-    id: objeto.querySelector(".btn-dark").dataset.id,
+    id: objeto.querySelector(".btn-info").dataset.id,
     title: objeto.querySelector("h5").textContent,
     precio: objeto.querySelector("p").textContent,
     cantidad: 1,
@@ -155,7 +155,7 @@ const pintarCard = (data) => {
     templateCard
       .querySelector("img")
       .setAttribute("src", producto.thumbnailUrl);
-    templateCard.querySelector(".btn-dark").dataset.id = producto.id;
+    templateCard.querySelector(".btn-info").dataset.id = producto.id;
 
     const clone = templateCard.cloneNode(true);
 
